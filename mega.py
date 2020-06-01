cbe779b6371bc8955d970821e36273f6fa70e7c4
Error killer <63567431+errorki11er@users.noreply.github.com>
2020-06-01 10:41:15 +0530

    Update mega.py

--- mega.py
+++ mega.py
@@ -13,7 +13,7 @@ ___________________________________________________________
 \033[1;34;40m     |  \/  | ___  __ _   / \    |  _ \ _   _| \ | |
 \033[1;36;40m     | |\/| |/ _ \/ _` | / O \   | |_) | | | |  \| |
 \033[1;34;40m     | |  | |  __/ (_| |/ ___ \  |  _ <| |_| | |\  |
-\033[1;36;40m     |_|  |_|\___|\__, /_/   \_\ |_| \_\\____/|_| \_|
+\033[1;36;40m     |_|  |_|\___|\__, /_/   \_\ |_| \_\\___/|_| \_|
 \033[1;34;40m                  |___/
 
 \033[1;35;40m              [+] Tool By ERROR KILLER
@@ -25,7 +25,7 @@ print(name, "")
 try:
     f = open("auth.txt", "r")
     auth = f.read()
-    f.close 
+    f.close
 except:
     wr = str(input("\033[1;0;40mPaste Your Auth here :- "))
     f = open("auth.txt", "w")
@@ -64,22 +64,19 @@ except ImportError:
 def main():
     os.system("clear")
     print(name,"\n")
-    s = int(input("\033[1;0;40mEnter Amount - "))
     header = {"Host": "megarun.dialog.lk",
               "Authorization": auth, "X-Unity-Version": "2018.3.0f2"}
     url = url1
-    
-    ss = 0
-    while s > ss:
-        os.system("clear")
-        print(name)
+    ss=0
+    time.sleep(90)
+    while True:
         size = 0
         res = requests.get(url, headers=header)
         resp = str(res)
         if resp == '<Response [204]>':
             print(bar)
             print("\n\033[1;32;40m [+] No Data ... [+]")
-            print(bar)  
+            print(bar)
         elif resp == '<Response [200]>':
             print(bar)
             print("\n\033[1;32;40m [+] You Won Check Balance ... [+]")
@@ -88,18 +85,17 @@ def main():
             print(bar)
             print("\n\033[1;31;40m [+] Check Again, I think You are Blocked User... [+]")
             print(bar)
-        
 
         ss+=1
         print("\033[1;0;40m\n",str(ss), "Please Wait For Next request",end="")
         for i in range(180):
-            
+
             pr = i/180*100
             print("\033[1;36;40m\n>>> [+]",str(int(pr)) +"% ",end="")
-            
+
             time.sleep(0.5)
             sys.stdout.write("\033[F")
-        
+
     os.system('figlet FINISHED')
     again()
 
